11a93157dafe1054d6b0a2d73c55c4429393e190
laobameishijia <994291043@qq.com>
2020-10-16 09:03:55 +0800

    Initial commit

---
+++ src/main/java/MiniprogramServer.java
@@ -0,0 +1,30 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import servlet.CalculateServlet;
+import servlet.HelloServlet;
+//服务器的基本配置文件
+public class MiniprogramServer {
+
+    public static void main(String[] args) {
+
+        try {
+            // 1、配置端口
+            int port = 8888;
+            Server server = new Server(port);
+
+            // 2、配置服务器参数
+            WebAppContext context = new WebAppContext();
+            context.setContextPath("/");
+            context.setResourceBase("web"); // 指定根目录
+            server.setHandler(context);
+            //差不多就是服务器端所部署的服务
+            context.addServlet(HelloServlet.class, "/hello");
+            context.addServlet(CalculateServlet.class,"/calc");
+
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/servlet/CalculateServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;//alt+shift+enter自动加入
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CalculateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String a = req.getParameter("a");
+        String b = req.getParameter("b");
+        int intA = 0;
+        int intB = 0;
+
+        if (a != null) {
+            intA = Integer.parseInt(a);
+        }
+        if (b != null) {
+            intB = Integer.parseInt(b);
+        }
+        int c = intA+intB;
+        String result = "{\"result\":"+c+"}";
+        resp.getWriter().println(result);
+
+    }
+}
